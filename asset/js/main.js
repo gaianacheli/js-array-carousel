ffed8e1ce4178ff7767edc5b920b9056879e9659
gaianacheli <gaianacheli04@gmail.com>
2021-10-15 19:13:57 +0200

    esercizio completo

--- asset/js/main.js
+++ asset/js/main.js
@@ -21,3 +21,55 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+const btnArrowUp = document.querySelector('.frecciaup')
+const btnArrowDown = document.querySelector('.frecciadown')
+const selectedImage = document.getElementById('selectedImage')
+const titolo = document.getElementById('titolo')
+const sottotitolo = document.getElementById('sottotitolo')
+const imgOne = document.getElementById('imgone')
+const listImages =  document.getElementById('listImage')
+
+let selected =0
+
+for (let i = 0; i < items.length; i++) {
+    const newBox = `<div class="boxstyle unselectedImg" id="${i}"><img class="imgstyle" src="./asset/${items[i]}" alt="pippo"></div>`;
+    
+    listImages.insertAdjacentHTML('beforeend',newBox);
+}
+
+
+
+imgOne.src=`./asset/${items[selected]}`;
+titolo.innerHTML=title[selected];
+sottotitolo.innerHTML=text[selected];
+let selectedImg=document.getElementById(selected);
+selectedImg.className="boxstyle selectedImg"
+
+
+btnArrowDown.addEventListener('click', function(){
+    selectedImg=document.getElementById(selected);
+    selectedImg.className="boxstyle unselectedImg"
+    selected++
+    if (selected==5){
+        selected=0
+    }
+    imgOne.src=`./asset/${items[selected]}`
+    titolo.innerHTML=title[selected]
+    sottotitolo.innerHTML=text[selected]
+    selectedImg=document.getElementById(selected);
+    selectedImg.className="boxstyle selectedImg"
+})
+
+btnArrowUp.addEventListener('click', function(){
+    selectedImg=document.getElementById(selected);
+    selectedImg.className="boxstyle unselectedImg"
+    selected--
+    if (selected == -1){
+        selected=4
+    }
+    imgOne.src=`./asset/${items[selected]}`
+    titolo.innerHTML=title[selected]
+    sottotitolo.innerHTML=text[selected]
+    selectedImg=document.getElementById(selected);
+selectedImg.className="boxstyle selectedImg"
+})
